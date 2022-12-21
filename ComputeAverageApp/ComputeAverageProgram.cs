3f597e7f3444d5cdd2e9772d029c42b38d0c7403
Wrichmond Well A. Chua <59139222+WrichmondChua@users.noreply.github.com>
2022-12-21 18:17:13 +0800

    Add files via upload

---
+++ ComputeAverageApp/ComputeAverageProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            double grades1, grades2, grades3, grades4, grades5;
+            Console.WriteLine("Enter 5 grades separated by new line: ");
+            grades1 = Convert.ToDouble(Console.ReadLine());
+            grades2 = Convert.ToDouble(Console.ReadLine());
+            grades3 = Convert.ToDouble(Console.ReadLine());
+            grades4 = Convert.ToDouble(Console.ReadLine());
+            grades5 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = grades1 + grades2 + grades3 + grades4 + grades5;
+            double average = sum / 5;
+            double RoundOff = Math.Round(average);
+            Console.WriteLine("The average is " + average + " and round off to " + RoundOff);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+
+        }
+    }
+}
