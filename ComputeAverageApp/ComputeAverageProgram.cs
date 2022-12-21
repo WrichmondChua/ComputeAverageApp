8672c4503270aae3ac8045918c1d103d3d5d4b31
Wrichmond Well A. Chua <bangmondcurry@gmail.com>
2022-12-21 18:25:04 +0800

    Quick Update
    I added two Console.WriteLine on lines 16 and 23 in order to practice myself on learning how to use GitHub, which is very essential for my future career.

--- ComputeAverageApp/ComputeAverageProgram.cs
+++ ComputeAverageApp/ComputeAverageProgram.cs
@@ -13,12 +13,14 @@ namespace ComputeAverageApp
             grades3 = Convert.ToDouble(Console.ReadLine());
             grades4 = Convert.ToDouble(Console.ReadLine());
             grades5 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("I like to drink Apple Juice.");
 
             double sum = grades1 + grades2 + grades3 + grades4 + grades5;
             double average = sum / 5;
             double RoundOff = Math.Round(average);
             Console.WriteLine("The average is " + average + " and round off to " + RoundOff);
             Console.WriteLine("Press any key to exit...");
+            Console.WriteLine("Hello World");
             Console.ReadLine();
 
         }
